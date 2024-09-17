05dac1e2eaa3379824ea09bd9fa83a28b7bbbd1b
Kingrexicon <oyedeleoreofe@gmail.com>
2024-09-17 21:33:41 +0100

    started something called TAPJOSH

---
+++ includes/function.php
@@ -0,0 +1,17 @@
+<?php
+
+    $test = (int)14;
+
+    echo $test;
+   
+
+    echo "work";
+
+    function pointsincrease(){
+        $i =0;
+        whi($i=0; $i>=0; $i+1){
+            echo $i;
+            die();
+        }
+    }
+ 
---
+++ index.php
@@ -0,0 +1,79 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TAPJOSH</title>
+    <link rel="stylesheet" href="./styles/style.css">
+    <link rel="stylesheet" href="./styles/reset.css">
+
+</head>
+
+<body>
+
+<nav>
+
+</nav>
+
+<div class="main">
+    <input type="submit" value="" name="point" class="circle"></input>
+</div>
+
+
+<footer>
+
+</footer>
+
+
+  
+<!-- <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST"> -->
+
+
+<!-- <input type="submit" value="Click me" name="increasePoint"> -->
+
+    <!-- <butto name ="increasePoinnts" >click</button> -->
+</form>
+
+<?php
+// $point = 1;
+// echo $point."<br>";
+
+
+// if(!isset($_POST['increasePoint'])){
+// echo "no points";
+
+
+// }else{
+//     $point+1;
+//     echo "points";
+// }
+
+
+// function increase_points(){
+//     $point = 1;
+//     $points = 0;;
+
+//     while($point > 1){
+//         echo $points;
+//         echo "<br>";
+//         $points + 1;
+        
+//     }
+// }
+
+// echo $point; 
+
+// function btn(){
+//     global $point, $points;
+//     echo $points;
+//     die;
+// }
+
+
+?>
+
+</body>
+
+</html>
